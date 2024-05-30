81088aa872bb9573a0e5c398b3a4ac36070936e0
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-30 18:43:37 +0200

    Corrige nombres de variables y columnas para el nuevo front con React

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,8 @@ const { pool } = require('../db');
 const userRegister = async (req, res) => {
     try {
         console.log(req.body);
-        let params = [req.body.name, req.body.last_name, req.body.email, req.body.photo, req.body.password];
-        let sql = `INSERT INTO user (name, last_name, email, photo, password) 
+        let params = [req.body.firstName, req.body.lastName, req.body.email, req.body.photo, req.body.password];
+        let sql = `INSERT INTO user (firstName, lastName, email, photo, password) 
                     VALUES (?, ?, ?, ?, ?)`;
         console.log(sql);
 
@@ -32,8 +32,8 @@ const userLogin = async (req, res) => {
         if (result.length > 0) {
             const userData = {
                 id_user: result[0].id_user,
-                name: result[0].name,
-                last_name: result[0].last_name,
+                firstName: result[0].firstName,
+                lastName: result[0].lastName,
                 email: result[0].email,
                 photo: result[0].photo
             };
@@ -48,9 +48,9 @@ const userLogin = async (req, res) => {
 
 const editUser = async (req, res) => {
     try {
-        let params = [req.body.name, req.body.last_name, req.body.photo, req.body.email];
+        let params = [req.body.firstName, req.body.lastName, req.body.photo, req.body.email];
         let sql = `UPDATE user 
-                    SET name = COALESCE(?, name), last_name = COALESCE(?, last_name), 
+                    SET first_name = COALESCE(?, firstName), last_name = COALESCE(?, lastName), 
                     photo = COALESCE(?, photo)   
                     WHERE email = ?`;
         let [result] = await pool.query(sql, params);
