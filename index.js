9ed8c7abbdc586b3edd36de4b144d41c9da78b8d
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-25 14:55:37 +0200

    Add corrections for vercel deployment

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// import {listen, get} from './src/app.js';
+import { listen } from "./app.js";
 
-// listen(get('port'), () => {
-//     console.log('Server listen on port ' + get('port'));
-// });
-
-const app = require("./app");
-
-app.listen(process.env.PORT || 3000);
+listen(process.env.PORT || 3000);
